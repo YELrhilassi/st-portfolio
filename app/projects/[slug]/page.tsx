693e1b90343b014036b7214d442000baa9b25fe4
Youssef El Rhilassi <elrhilassiy@gmail.com>
2025-09-09 15:29:11 -0700

    modules fix

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,11 +1,4 @@
-import { fetchProjects } from "@/lib/data/fetchProjects";
 
-export async function generateStaticParams() {
-  const projects = await fetchProjects();
-  return projects.map((project) => ({
-    slug: project.slug,
-  }));
-}
 
 export default async function Projects(params:any){
 
